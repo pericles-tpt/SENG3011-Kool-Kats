ef34b9a8e4fc0964d2c630bdde1041d90ae6c7c3
Stephen Comino <scomino@gmail.com>
2021-03-11 15:56:07 +1100

    Added diseases and Countries

--- pychromeless/src/lambda_function.py
+++ pychromeless/src/lambda_function.py
@@ -2,16 +2,54 @@ import time
 
 from webdriver_wrapper import WebDriverWrapper
 from selenium.webdriver.common.keys import Keys
-
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def lambda_handler(*args, **kwargs):
-    
+
+   
+    countries = get_countries()
+    diseases = get_diseases()
+
+    print('diseases ' + str(diseases))
+    print("Space")
+    print('countries ' + str(countries))
+
+
+    return None
+
+def get_diseases():
     driver = WebDriverWrapper()
-    #driver.get("https://www.python.org")
 
-    ### Fill in with scrape code
+    driver.get_url("https://www.who.int/csr/don/archive/disease/en/")
 
-    text = "Here it is me"
+    element_list = driver.find_name('col_2-1_1')
+
+    tags = element_list.find_elements_by_tag_name('a')
+
+    diseases = []
+
+    for items in tags:
+        if len(items.text) > 1:
+            diseases.append(items.text)
     driver.close()
+    return diseases
 
-    return text
+def get_countries():
+    driver = WebDriverWrapper()
+
+    driver.get_url("https://www.who.int/csr/don/archive/country/en/")
+
+    element_list = driver.find_name('col_2-1_1')
+
+    tags = element_list.find_elements_by_tag_name('a')
+
+    countries = []
+
+    for items in tags:
+        if len(items.text) > 1 and items.text != "Back to Top":
+            countries.append(items.text)
+    driver.close()
+    return countries
