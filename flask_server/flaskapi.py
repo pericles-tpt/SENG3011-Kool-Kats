2424c5248b4cde5508bc4035e56276d02a618ea4
Pericles Telemachou <pericles@outlook.com.au>
2021-03-16 20:25:57 +1100

    Added setup.py, flask server implemented

---
+++ flask_server/flaskapi.py
@@ -0,0 +1,14 @@
+from flask import Flask, json
+import sys
+import pychromeless.src.db_functions
+
+api = Flask(__name__)
+
+test = {"a":True, "b":"hi","c":1,"d":[1,2,3,4,5]}
+
+@api.route('/articles', methods=['GET'])
+def get_articles():
+    return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2019-12-31', None, 'measles'))
+
+if __name__ == '__main__':
+    api.run()
