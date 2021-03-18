f9a0f7cb57f9e3ab45c969dc0eda26d4adf480aa
Pericles Telemachou <pericles@outlook.com.au>
2021-03-18 16:20:37 +1100

    More request resp tests

--- flask_server/flaskapi.py
+++ flask_server/flaskapi.py
@@ -18,6 +18,37 @@ def get_articles():
     return (startDate + " " + endDate + " " + keyTerms + " " + location)
     #return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
 
+@api.route('/diseases', methods=['GET'])
+@cross_origin()
+def get_diseases():
+    startDate = request.args.get('startDate')
+    endDate = request.args.get('endDate')
+    keyTerms = request.args.get('keyTerms')
+    location = request.args.get('location')
+    #return (startDate + " " + endDate + " " + keyTerms + " " + location) 
+    return (startDate + " " + endDate + " " + keyTerms + " " + location)
+    #return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
+
+@api.route('/occurences', methods=['GET'])
+@cross_origin()
+def get_occurences():
+    keyTerms = request.args.get('keyTerms')
+    #return (startDate + " " + endDate + " " + keyTerms + " " + location) 
+    return (keyTerms)
+    #return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
+
+@api.route('/popularDiseases', methods=['GET'])
+@cross_origin()
+def get_popular_diseases():
+    startDate = request.args.get('startDate')
+    endDate = request.args.get('endDate')
+    numDiseases = request.args.get('numDiseases')
+    location = request.args.get('location')
+    #return (startDate + " " + endDate + " " + keyTerms + " " + location) 
+    return (startDate + " " + endDate + " " + keyTerms + " " + location)
+    #return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
+
+
 if __name__ == '__main__':
     api.run(host= '0.0.0.0')
     # Changed from app.run()
