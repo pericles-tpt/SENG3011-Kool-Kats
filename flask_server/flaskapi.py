2d8d7445c97f1c4f9f2a0824ec0d3f1fa8c86946
Pericles Telemachou <pericles@outlook.com.au>
2021-03-18 17:43:41 +1100

    Implemented rest of the requests

--- flask_server/flaskapi.py
+++ flask_server/flaskapi.py
@@ -14,9 +14,8 @@ def get_articles():
     endDate = request.args.get('endDate')
     keyTerms = request.args.get('keyTerms')
     location = request.args.get('location')
-    #return (startDate + " " + endDate + " " + keyTerms + " " + location) 
-    return (startDate + " " + endDate + " " + keyTerms + " " + location)
-    #return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
+
+    return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
 
 @api.route('/disease', methods=['GET'])
 @cross_origin()
@@ -25,17 +24,17 @@ def get_diseases():
     endDate = request.args.get('endDate')
     keyTerms = request.args.get('keyTerms')
     location = request.args.get('location')
-    #return (startDate + " " + endDate + " " + keyTerms + " " + location) 
-    return (startDate + " " + endDate + " " + keyTerms + " " + location)
-    #return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
+
+    return json.dumps(pychromeless.src.db_functions.handle_get_diseases(startDate, endDate, location, keyTerms))
 
 @api.route('/occurrences', methods=['GET'])
 @cross_origin()
 def get_occurences():
     keyTerms = request.args.get('keyTerms')
-    #return (startDate + " " + endDate + " " + keyTerms + " " + location) 
-    return (keyTerms)
-    #return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
+    startDate = request.args.get('startDate')
+    endDate = request.args.get('endDate')
+
+    return json.dumps(pychromeless.src.db_functions.handle_get_occurrences(startDate, endDate, keyTerms))
 
 @api.route('/popularDiseases', methods=['GET'])
 @cross_origin()
@@ -45,8 +44,8 @@ def get_popular_diseases():
     numDiseases = request.args.get('numDiseases')
     location = request.args.get('location')
     #return (startDate + " " + endDate + " " + keyTerms + " " + location) 
-    return (startDate + " " + endDate + " " + numDiseases + " " + location)
-    #return json.dumps(pychromeless.src.db_functions.handle_get_articles('2019-01-01', '2021-01-01', 'France', None))
+    #return (startDate + " " + endDate + " " + numDiseases + " " + location)
+    return json.dumps(pychromeless.src.db_functions.handle_get_popular_diseases(startDate, endDate, location, numDiseases))
 
 
 if __name__ == '__main__':
--- pychromeless/src/db_functions.py
+++ pychromeless/src/db_functions.py
@@ -37,6 +37,87 @@ def handle_get_articles(date_start, date_end, country = None, disease = None):
     
     return response
 
+def handle_get_diseases(date_start, date_end, country = None, keyTerms = None):
+    import pymysql
+    db = pymysql.connect(host="database-1.cmae6p4l3uws.us-east-1.rds.amazonaws.com",user="admin",db="scrape_db" , password="koolkats", port=3306)
+    cursor = db.cursor()
+
+    # Creates the WHERE part of the query
+    filters = []
+    where_query = ''
+    if country != None:
+        filters.append('Country=\'' + country + '\'')
+    elif disease != None:
+        filters.append('Disease=\'' + disease + '\'')
+    if len(filters) > 0:
+        where_query = 'AND '
+        for i in range(len(filters)):
+            if i == 0:
+                where_query += filters[i]
+            else:
+                where_query += ' AND ' + filters[i]
+
+    try:
+      query = "SELECT DISEASE, SUM(CASES) AS CASES FROM Articles WHERE Date >= '{}' AND Date <= '{}' {} GROUP BY DISEASE ORDER BY CASES DESC;".format(date_start, date_end, where_query)
+      print(query)
+      cursor.execute(query)
+    except:
+      print("Oops we had an error")
+      return
+
+    response = []
+    for (Disease, Cases) in cursor:
+        response.append({"Disease": Disease, "Cases": Cases})
+    
+    return response
+
+def handle_get_occurrences(startDate = None, endDate = None, keyTerms):
+    import pymysql
+    db = pymysql.connect(host="database-1.cmae6p4l3uws.us-east-1.rds.amazonaws.com",user="admin",db="scrape_db" , password="koolkats", port=3306)
+    cursor = db.cursor()
+
+    where_query = ''
+    if (startDate != None and endDate != None):
+        where_query = "WHERE Date >= '{}' AND Date <= '{}'".format(startDate, endDate) 
+
+    try:
+      query = "SELECT Country, Disease, COUNT(DISTINCT Url) AS Count FROM Articles " + where_query + " GROUP BY Country, Disease ORDER BY Count DESC;".format(date_start, date_end, where_query)
+      print(query)
+      cursor.execute(query)
+    except:
+      print("Oops we had an error")
+      return
+
+    response = {"locations":[]}
+    for (Country, Disease, Count) in cursor:
+        response["locations"].append({"name": Country, "disease": Disease, "occurrences": Count})
+    
+    return response
+
+def handle_get_popular_diseases(startDate, endDate, country = None, numDiseases = 10):
+    import pymysql
+    db = pymysql.connect(host="database-1.cmae6p4l3uws.us-east-1.rds.amazonaws.com",user="admin",db="scrape_db" , password="koolkats", port=3306)
+    cursor = db.cursor()
+
+    # Creates the WHERE part of the query
+    where_query = ''
+    if country != None:
+        where_query = 'WHERE Country=\'' + country + '\''
+
+    try:
+      query = "SELECT DISEASE, COUNT(DISTINCT URL) AS COUNT FROM Articles {} GROUP BY DISEASE ORDER BY COUNT DESC LIMIT {}".format(where_query, numDiseases)
+      print(query)
+      cursor.execute(query)
+    except:
+      print("Oops we had an error")
+      return
+
+    response = {"rankings": []}
+    for (Disease, Cases) in cursor:
+        response["rankings"].append({"name": Disease, "occurrences": Cases})
+    
+    return response
+
 
 def send_to_sql(articles):
     import pymysql
