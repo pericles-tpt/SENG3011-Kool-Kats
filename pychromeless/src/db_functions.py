5643fc40f884ce5d4f83cc0c28d89de8c598c4ef
Ubuntu <ubuntu@ip-172-31-35-247.ec2.internal>
2021-03-18 08:44:25 +0000

    Various fixes

--- pychromeless/src/db_functions.py
+++ pychromeless/src/db_functions.py
@@ -2,7 +2,7 @@ import time
 import json # STEPHEN: I added this
 import datetime
 
-def handle_get_articles(date_start, date_end, country = None, disease = None):
+def handle_get_articles(date_start, date_end, country = None, keyTerms = None):
     import pymysql
     db = pymysql.connect(host="database-1.cmae6p4l3uws.us-east-1.rds.amazonaws.com",user="admin",db="scrape_db" , password="koolkats", port=3306)
     cursor = db.cursor()
@@ -12,8 +12,9 @@ def handle_get_articles(date_start, date_end, country = None, disease = None):
     where_query = ''
     if country != None:
         filters.append('Country=\'' + country + '\'')
-    elif disease != None:
-        filters.append('Disease=\'' + disease + '\'')
+    if keyTerms != None:
+        keyTerms = keyTerms.split(',')
+        filters.append('Disease IN (\'' + '\', \''.join([term.strip() for term in keyTerms]) + '\')')
     if len(filters) > 0:
         where_query = 'AND '
         for i in range(len(filters)):
@@ -31,8 +32,8 @@ def handle_get_articles(date_start, date_end, country = None, disease = None):
       return
 
     response = []
-    for (Country, Disease, Date, Cases, Url, MainText) in cursor:
-        response.append({"headline": "test", "url": Url, "location": Country, "reports": ["a", "b", "c"], "termsFound": Disease, "main_text": MainText, "date_of_publication": str(Date)})
+    for (Headline, Country, Disease, Date, Cases, Url, MainText) in cursor:
+        response.append({"headline": Headline, "url": Url, "location": Country, "reports": ["a", "b", "c"], "termsFound": Disease, "main_text": MainText, "date_of_publication": str(Date)})
 
     
     return response
@@ -48,7 +49,8 @@ def handle_get_diseases(date_start, date_end, country = None, keyTerms = None):
     if country != None:
         filters.append('Country=\'' + country + '\'')
     elif keyTerms != None:
-        filters.append('Disease IN (\'' + '\', \''.join(keyTerms) + '\')')
+        keyTerms = keyTerms.split(',')
+        filters.append('Disease IN (\'' + '\', \''.join([term.strip() for term in keyTerms]) + '\')')
     if len(filters) > 0:
         where_query = 'AND '
         for i in range(len(filters)):
@@ -58,16 +60,24 @@ def handle_get_diseases(date_start, date_end, country = None, keyTerms = None):
                 where_query += ' AND ' + filters[i]
 
     try:
-      query = "SELECT DISEASE, SUM(CASES) AS CASES FROM Articles WHERE Date >= '{}' AND Date <= '{}' {} GROUP BY DISEASE ORDER BY CASES DESC;".format(date_start, date_end, where_query)
+      query = "SELECT DISEASE, SUM(CASES) AS CASES, COUNT(CASES) AS AF FROM Articles WHERE Date >= '{}' AND Date <= '{}' {} GROUP BY DISEASE ORDER BY CASES DESC;".format(date_start, date_end, where_query)
       print(query)
       cursor.execute(query)
     except:
       print("Oops we had an error")
       return
 
-    response = []
-    for (Disease, Cases) in cursor:
-        response.append({"Disease": Disease, "Cases": int(Cases)})
+    tmpList = []
+    for (Disease, Cases, af) in cursor:
+        tmpList.append({"diseaseInfo": {"name": Disease, "occurences": int(Cases), "articlesFound": int(af)}})
+
+    query = "SELECT COUNT(*) AS TA FROM Articles;"
+    cursor.execute(query)
+
+    for i in cursor:
+        ta = i[0]
+
+    response = {"diseases":tmpList, "totalArticles": ta}
     
     return response
 
@@ -75,13 +85,17 @@ def handle_get_occurrences(keyTerms, startDate = None, endDate = None):
     import pymysql
     db = pymysql.connect(host="database-1.cmae6p4l3uws.us-east-1.rds.amazonaws.com",user="admin",db="scrape_db" , password="koolkats", port=3306)
     cursor = db.cursor()
+    keyTerms = keyTerms.split(',')    
+    for i in keyTerms:
+        print(i)
 
-    where_query = 'Disease IN (\'' + '\', \''.join(keyTerms) + '\')'
+
+    where_query = 'WHERE Disease IN (\'' + '\', \''.join([term.strip() for term in keyTerms]) + '\')'
     if (startDate != None and endDate != None):
         where_query = " AND Date >= '{}' AND Date <= '{}'".format(startDate, endDate) 
 
     try:
-      query = "SELECT Country, Disease, COUNT(DISTINCT Url) AS Count FROM Articles " + where_query + " GROUP BY Country, Disease ORDER BY Count DESC;".format(date_start, date_end, where_query)
+      query = "SELECT Country, Disease, COUNT(DISTINCT Url) AS Count FROM Articles " + where_query + " GROUP BY Country, Disease ORDER BY Count DESC;".format(where_query)
       print(query)
       cursor.execute(query)
     except:
