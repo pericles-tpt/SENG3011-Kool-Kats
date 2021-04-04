6e89cfe661dfa22e1ff3af734b4dc52ff0b91c20
Joanne <joannechen84@gmail.com>
2021-04-04 15:22:45 +1000

    fixed date range

--- PHASE_2/Application_SourceCode/koolkats-site/src/components/ArticlesModal.js
+++ PHASE_2/Application_SourceCode/koolkats-site/src/components/ArticlesModal.js
@@ -6,37 +6,56 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Spinner from 'react-bootstrap/Spinner'
 import "bootstrap/dist/css/bootstrap.min.css";
+import getArticles from './RequestData'
 
-const ArticlesModal = ({show, handleClose, country, disease, timeRange}) => {
+const ArticlesModal = ({show, handleClose, location, disease, startDate, endDate}) => {
     // article modal parent object
     /*
     const [show, setShow] = useState(true);
     const handleClose = () => setShow(false);
+    <ArticlesModal handleClose={handleClose} show={show} location="Australia" disease={selectedDiseases} startDate={startDate} endDate={endDate}/>
     */
     const [articles, setArticles] = useState([])
     const [showSpinner, setShowSpinner] = useState('block')
     useEffect(() => {
-        setArticles([{'title': "article title", 'link': 'this is the article link', 'date': '3/04/2021'},{'title': "article title 2", 'link': 'this is another article link', 'date': '4/04/2021'}])
+        //const articlesFound = getArticles(startDate, endDate, disease, location)
+        //console.log('articles found: ' + articlesFound)
+        //setArticles(articlesFound)
+        setArticles([{'headline': "article title", 'url': 'this is the article link', 'date_of_publication': '3/04/2021'},{'headline': "article title 2", 'url': 'this is another article link', 'date_of_publication': '4/04/2021'}])
         setShowSpinner('none')
     }, [])
     return (
-        <Modal size="lg" centred={true} show={show} onHide={handleClose}>
+        <Modal 
+            size="lg" 
+            centred={true} 
+            show={show} 
+            onHide={handleClose}
+        >
             <Modal.Header closeButton>
                 <Container>
                     <Row>
-                        <Modal.Title style={{padding: '15px', textAlign: 'center'}}>
+                        <Modal.Title 
+                            style={{
+                                padding: '15px', 
+                                textAlign: 'center'
+                            }}
+                        >
                             Articles for
                         </Modal.Title>
                     </Row>
                     <Row>
-                        <Col>Location: {country}</Col>
-                        <Col>Disease: {disease}</Col>
-                        <Col>Time Period: {timeRange.start} - {timeRange.end}</Col>
+                        <Col>Location: {location}</Col>
+                        <Col>Disease: {disease.join(', ')}</Col>
+                        <Col>Time Period: {startDate.toISOString().split('T')[0]} - {endDate.toISOString().split('T')[0]}</Col>
                     </Row>
                 </Container>
             </Modal.Header>
             <Modal.Body scrollable={true}>
-                <Spinner animation="border" role="status" style={{display: showSpinner}}>
+                <Spinner 
+                    animation="border" 
+                    role="status" 
+                    style={{display: showSpinner}}
+                >
                     <span className="sr-only">Getting Articles...</span>
                 </Spinner>
                 {articles.map((article) => (<Article article={article}/>))}
@@ -49,23 +68,24 @@ const ArticlesModal = ({show, handleClose, country, disease, timeRange}) => {
         </Modal> 
     );
   };
-const style = {
-    boxShadow: '2px 2px 1px grey',
-    border: '1px solid grey',
-    margin: 'auto',
-    padding: '10px'
-}
 
 const Article = ({article}) => {
-    console.log("article:" + article)
     return (
-        <Container fluid={true} style={style}>
+        <Container 
+            fluid={true} 
+            style={{
+                boxShadow:'2px 2px 1px grey', 
+                border: '1px solid grey',
+                margin: 'auto',
+                padding: '10px'
+            }}
+        >
             <Row>
-                <Col md="auto">{article.title}</Col>
-                <Col>{article.date}</Col>
+                <Col md="auto">{article.headline}</Col>
+                <Col>{article.date_of_publication}</Col>
             </Row>
             <Row>
-                <Col>{article.link}</Col>
+                <Col>{article.url}</Col>
             </Row>
         </Container>
     );
