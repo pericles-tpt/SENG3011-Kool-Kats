4463d9563e6218c394a4de07d0cea834925c7572
Stephen Comino <scomino@gmail.com>
2021-04-18 10:33:36 +1000

    added Covid to heat map

--- PHASE_2/Application_SourceCode/koolkats-site/src/components/MapChart.js
+++ PHASE_2/Application_SourceCode/koolkats-site/src/components/MapChart.js
@@ -9,7 +9,8 @@ import {
   Sphere,
   Graticule,
 } from "react-simple-maps";
-import { getOccurrences } from "./RequestData";
+import { getOccurrences, getCOVIDCasesCountries } from "./RequestData";
+import { time } from "highcharts";
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
@@ -47,19 +48,25 @@ const MapChart = ({
     }
 
     console.log("fetching data for map");
-    fetchData(diseases, start, end);
+    var covid = false;
+    for (var j = 0; j < selectedDiseases.length; j++) {
+      if (selectedDiseases[j] === "COVID-19") {
+        covid = true;
+      }
+    }
+    fetchData(diseases, start, end, covid);
   }, [selectedDiseases, startDate, endDate]);
 
-  async function fetchData(diseases, start, end) {
-    await getOccurrences(diseases, start, end)
+  async function fetchData(diseases, start, end, covid) {
+    var results = await getOccurrences(diseases, start, end)
       .then((res) => {
         // Set Max
-        console.log(res.locations);
+        //console.log(res.locations);
         var tempLocation = res.locations;
         var length = tempLocation.length;
         var tempMax = 1;
         //setMax(1);
-        console.log(length);
+        //console.log(length);
         for (var i = 0; i < length; i++) {
           if (tempLocation[i]["cases"] > tempMax) {
             tempMax = tempLocation[i]["cases"];
@@ -68,7 +75,81 @@ const MapChart = ({
         }
         setMax(tempMax);
         setData(res.locations);
-      })
+        var countries = [];
+        for (var i = 0; i < length; i++) {
+          countries[i] = tempLocation[i]["name"];
+        }
+        console.log(tempLocation);
+        console.log(countries);
+        var countriesQuery = countries.join(",");
+        console.log(countriesQuery);
+        const date1 = start;
+        const date2 = end;
+
+        const DAY_UNIT_IN_MILLISECONDS = 24 * 3600 * 1000;
+
+        const diffInMilliseconds = new Date(date1).getTime() - new Date(date2).getTime();
+        const diffInDays = diffInMilliseconds / DAY_UNIT_IN_MILLISECONDS;
+        var noDays = Math.floor(Math.abs(diffInDays));
+        var results = {'Days': noDays, 'Countries': countriesQuery};
+        return results;
+        
+      });
+      var countriesQuery = results['Countries'];
+      var noDays = results['Days'];
+      if (covid) {
+        await getCOVIDCasesCountries(countriesQuery, noDays).then(res => {
+            //console.log(res);
+            var timeline_length = res.length;
+            var timeline = {};
+            if (timeline_length > 1) {
+              for (var i = 0; i < timeline_length; i++) {
+                
+                if (!(res[i] == null)) {
+                  //console.log(res[i]);
+                  timeline[res[i]['country']] = res[i]['timeline']['cases'];
+                  //timeline.push(res[i]['timeline']['cases']);
+                }
+              }
+          } else {
+            timeline[res[0]['country']] = res[0]['timeline']['cases'];
+          }
+
+            // Get total
+            var total = 0;
+            var total_length = timeline.length;
+            for (var key in timeline) {
+              var cases_length = timeline[key];
+              var total = 0;
+              for (var time in timeline[key]) {
+                total += timeline[key][time];
+              }
+              timeline[key]['total'] = total;
+              
+            }
+            console.log(timeline);
+          
+          var new_data = data;
+          console.log(new_data);
+
+            for (var d in timeline) {
+              var length = new_data.length;
+              if (!(d == undefined)) {
+              for (var i = 0; i < length; i++) {
+                console.log(new_data[i]["name"])
+                if (new_data[i]["name"] === d || String(new_data[i]["name"]),includes(d)) {
+                  console.log(timeline[d]['total']);
+                  new_data[i]["cases"] += timeline[d]['total'];
+                }
+              }
+            }
+            // Get data
+            setData(new_data);
+          }
+        });
+      }
+
+        //setState([...oldList, newItem]) Append a State
   }
 
   const color = scaleLinear().domain([0, 5]).range(["#ffedea", "#ff5233"]);
