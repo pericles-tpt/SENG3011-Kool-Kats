2f23a5ddd6c35c1f139d64230e2c066d25d6f5b8
Joanne <joannechen84@gmail.com>
2021-04-21 01:34:26 +1000

    added links to more info

--- PHASE_2/Application_SourceCode/koolkats-site/src/components/RestrictionsOverlay.js
+++ PHASE_2/Application_SourceCode/koolkats-site/src/components/RestrictionsOverlay.js
@@ -8,20 +8,22 @@ import {getStateRestrictionAus} from './RequestData'
 const RestrictionsOverlay = ({show}) => {
     const [restrictions, setRestrictions] = useState([{'...': 'Could not get restrictions'}])
     const [state, setState] = useState('NSW')
+    const [link, setLink] = useState('https://www.nsw.gov.au/covid-19/rules/border-restrictions')
     useEffect(() => {
         console.log('state changed ', state)
         async function getInfo() {
             console.log("in get info...")
             if (state === 'NSW') {
                 setRestrictions([
-                    {'ACT': 'ACT->NSW'}, 
-                    {'QLD': 'QLD->NSW'}, 
-                    {'VIC': 'VIC->NSW'},
-                    {'WA': 'WA->NSW'},
-                    {'SA': 'SA->NSW'},
-                    {'NT': 'NT->NSW'},
-                    {'TAS': 'TAS->NSW'}
+                    {'ACT': 'Open - no permit required'}, 
+                    {'QLD': 'Some areas in Queensland are considered affected areas. Read the advice for travellers from Queensland.'}, 
+                    {'VIC': 'Open - no permit required'},
+                    {'WA': 'Open - no permit required'},
+                    {'SA': 'Open - no permit required'},
+                    {'NT': 'Open - no permit required'},
+                    {'TAS': 'Open - no permit required'}
                 ])
+                setLink('https://www.nsw.gov.au/covid-19/rules/border-restrictions')
             } else if (state === 'ACT') {
                 setRestrictions([
                     {'NSW': 'NSW->ACT'}, 
@@ -32,6 +34,7 @@ const RestrictionsOverlay = ({show}) => {
                     {'NT': 'NT->ACT'},
                     {'TAS': 'TAS->ACT'}
                 ])
+                setLink('https://www.covid19.act.gov.au/community/travel')
             } else if (state === 'TAS') {
                 setRestrictions([
                     {'ACT': 'ACT->TAS'},
@@ -42,16 +45,18 @@ const RestrictionsOverlay = ({show}) => {
                     {'SA': 'SA->TAS'},
                     {'NT': 'NT->TAS'}
                 ])
+                setLink('https://coronavirus.tas.gov.au/travellers-and-visitors/coming-to-tasmania')
             } else if (state === 'VIC') {
                 setRestrictions([
-                    {'ACT': 'ACT->VIC'},
-                    {'NSW': 'NSW->VIC'}, 
-                    {'QLD': 'QLD->VIC'}, 
-                    {'WA': 'WA->VIC'},
-                    {'SA': 'SA->VIC'},
-                    {'NT': 'NT->VIC'},
-                    {'TAS': 'TAS->VIC'}
+                    {'ACT': 'Victoria has established a permit system for all domestic and New Zealand travel into Victoria. You must apply for a permit to enter Victoria from anywhere in Australia or New Zealand unless an exception or exemption applies to you.'},
+                    {'NSW': 'Victoria has established a permit system for all domestic and New Zealand travel into Victoria. You must apply for a permit to enter Victoria from anywhere in Australia or New Zealand unless an exception or exemption applies to you.'}, 
+                    {'QLD': 'Victoria has established a permit system for all domestic and New Zealand travel into Victoria. You must apply for a permit to enter Victoria from anywhere in Australia or New Zealand unless an exception or exemption applies to you.'}, 
+                    {'WA': 'Victoria has established a permit system for all domestic and New Zealand travel into Victoria. You must apply for a permit to enter Victoria from anywhere in Australia or New Zealand unless an exception or exemption applies to you.'},
+                    {'SA': 'Victoria has established a permit system for all domestic and New Zealand travel into Victoria. You must apply for a permit to enter Victoria from anywhere in Australia or New Zealand unless an exception or exemption applies to you.'},
+                    {'NT': 'Victoria has established a permit system for all domestic and New Zealand travel into Victoria. You must apply for a permit to enter Victoria from anywhere in Australia or New Zealand unless an exception or exemption applies to you.'},
+                    {'TAS': 'Victoria has established a permit system for all domestic and New Zealand travel into Victoria. You must apply for a permit to enter Victoria from anywhere in Australia or New Zealand unless an exception or exemption applies to you.'}
                 ])
+                setLink('https://www.coronavirus.vic.gov.au/travel-restrictions')
             } else if (state === 'QLD') {
                 setRestrictions([
                     {'ACT': 'ACT->QLD'},
@@ -62,6 +67,7 @@ const RestrictionsOverlay = ({show}) => {
                     {'NT': 'NT->QLD'},
                     {'TAS': 'TAS->QLD'}
                 ])
+                setLink('https://www.covid19.qld.gov.au/government-actions/border-closing')
             } else if (state === 'NT') {
                 setRestrictions([
                     {'ACT': 'ACT->NT'},
@@ -72,6 +78,7 @@ const RestrictionsOverlay = ({show}) => {
                     {'SA': 'SA->NT'},
                     {'TAS': 'TAS->NT'}
                 ])
+                setLink('https://coronavirus.nt.gov.au/travel/quarantine')
             } else if (state === 'SA') {
                 setRestrictions([
                     {'ACT': 'ACT->SA'},
@@ -82,6 +89,7 @@ const RestrictionsOverlay = ({show}) => {
                     {'NT': 'NT->SA'},
                     {'TAS': 'TAS->SA'}
                 ])
+                setLink('https://www.covid-19.sa.gov.au/restrictions-and-responsibilities/travel-restrictions')
             } else if (state === 'WA') {
                 setRestrictions([
                     {'ACT': 'ACT->WA'},
@@ -92,6 +100,7 @@ const RestrictionsOverlay = ({show}) => {
                     {'NT': 'NT->WA'},
                     {'TAS': 'TAS->WA'}
                 ])
+                setLink('https://www.wa.gov.au/organisation/department-of-the-premier-and-cabinet/covid-19-coronavirus-travel-and-quarantine')
             } 
             const res = await getStateRestrictionAus()
             setRestrictions(res)
@@ -106,7 +115,7 @@ const RestrictionsOverlay = ({show}) => {
                 display: show
             }}>
             <Row className="justify-content-md-center">
-                <DropdownButton id="dropdown-item-button" title="View State Restrictions">
+                <DropdownButton id="dropdown-item-button" title="View Restrictions to enter into States">
                     <Dropdown.Item as="button" onClick={() => setState('')}>None</Dropdown.Item>
                     <Dropdown.Item as="button" onClick={() => setState('ACT')}>ACT (Canberra)</Dropdown.Item>
                     <Dropdown.Item as="button" onClick={() => setState('NSW')}>New South Wales</Dropdown.Item>
@@ -121,11 +130,14 @@ const RestrictionsOverlay = ({show}) => {
             <Row className="justify-content-md-center">
                 {restrictions.map((rule, index) => (<RestrictionsForState num={index} stateTo={state} restriction={rule}/>))}
             </Row>
+            <Row className="justify-content-md-center">
+                For more information, view {link}
+            </Row>
         </Container>
     )
 }
 
-const RestrictionsForState = ({num, stateTo, restriction}) => {
+const RestrictionsForState = ({num, stateTo, restriction, link}) => {
     console.log(stateTo, restriction, num)
     const ruleNum = num + 1
     const state = Object.keys(restriction)
