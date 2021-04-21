709b1950613fb8e053e07665ba607ef2f72f9e51
Joanne <joannechen84@gmail.com>
2021-04-21 16:28:42 +1000

    added restrictions

--- PHASE_2/Application_SourceCode/koolkats-site/src/components/RestrictionsOverlay.js
+++ PHASE_2/Application_SourceCode/koolkats-site/src/components/RestrictionsOverlay.js
@@ -26,24 +26,24 @@ const RestrictionsOverlay = ({show}) => {
                 setLink('https://www.nsw.gov.au/covid-19/rules/border-restrictions')
             } else if (state === 'ACT') {
                 setRestrictions([
-                    {'NSW': 'NSW->ACT'}, 
-                    {'QLD': 'QLD->ACT'}, 
-                    {'VIC': 'VIC->ACT'},
-                    {'WA': 'WA->ACT'},
-                    {'SA': 'SA->ACT'},
-                    {'NT': 'NT->ACT'},
-                    {'TAS': 'TAS->ACT'}
+                    {'NSW': 'There are no requirements to advise ACT Health you are travelling or to quarantine.'}, 
+                    {'QLD': 'There are no requirements to advise ACT Health you are travelling or to quarantine.'}, 
+                    {'VIC': 'There are no requirements to advise ACT Health you are travelling or to quarantine.'},
+                    {'WA': 'There are no requirements to advise ACT Health you are travelling or to quarantine.'},
+                    {'SA': 'There are no requirements to advise ACT Health you are travelling or to quarantine.'},
+                    {'NT': 'There are no requirements to advise ACT Health you are travelling or to quarantine.'},
+                    {'TAS': 'There are no requirements to advise ACT Health you are travelling or to quarantine.'}
                 ])
                 setLink('https://www.covid19.act.gov.au/community/travel')
             } else if (state === 'TAS') {
                 setRestrictions([
-                    {'ACT': 'ACT->TAS'},
-                    {'NSW': 'NSW->TAS'}, 
-                    {'QLD': 'QLD->TAS'}, 
-                    {'VIC': 'VIC->TAS'},
-                    {'WA': 'WA->TAS'},
-                    {'SA': 'SA->TAS'},
-                    {'NT': 'NT->TAS'}
+                    {'ACT': 'Travellers to Tasmania, including returning residents, need to provide their contact and travel details before entering the State, to help manage the risk of COVID-19 at Tasmania’s borders.'},
+                    {'NSW': 'Travellers to Tasmania, including returning residents, need to provide their contact and travel details before entering the State, to help manage the risk of COVID-19 at Tasmania’s borders.'}, 
+                    {'QLD': 'Travellers to Tasmania, including returning residents, need to provide their contact and travel details before entering the State, to help manage the risk of COVID-19 at Tasmania’s borders.'}, 
+                    {'VIC': 'Travellers to Tasmania, including returning residents, need to provide their contact and travel details before entering the State, to help manage the risk of COVID-19 at Tasmania’s borders.'},
+                    {'WA': 'Travellers to Tasmania, including returning residents, need to provide their contact and travel details before entering the State, to help manage the risk of COVID-19 at Tasmania’s borders.'},
+                    {'SA': 'Travellers to Tasmania, including returning residents, need to provide their contact and travel details before entering the State, to help manage the risk of COVID-19 at Tasmania’s borders.'},
+                    {'NT': 'Travellers to Tasmania, including returning residents, need to provide their contact and travel details before entering the State, to help manage the risk of COVID-19 at Tasmania’s borders.'}
                 ])
                 setLink('https://coronavirus.tas.gov.au/travellers-and-visitors/coming-to-tasmania')
             } else if (state === 'VIC') {
@@ -59,46 +59,46 @@ const RestrictionsOverlay = ({show}) => {
                 setLink('https://www.coronavirus.vic.gov.au/travel-restrictions')
             } else if (state === 'QLD') {
                 setRestrictions([
-                    {'ACT': 'ACT->QLD'},
-                    {'NSW': 'NSW->QLD'}, 
-                    {'VIC': 'VIC->QLD'}, 
-                    {'WA': 'WA->QLD'},
-                    {'SA': 'SA->QLD'},
-                    {'NT': 'NT->QLD'},
-                    {'TAS': 'TAS->QLD'}
+                    {'ACT': 'You can enter Queensland from any Australian state or territory.There are no COVID-19 hotspots in Australia.The Queensland Border Declaration Pass system is not active. You do not need a border pass to enter Queensland.'},
+                    {'NSW': 'You can enter Queensland from any Australian state or territory.There are no COVID-19 hotspots in Australia.The Queensland Border Declaration Pass system is not active. You do not need a border pass to enter Queensland.'}, 
+                    {'VIC': 'You can enter Queensland from any Australian state or territory.There are no COVID-19 hotspots in Australia.The Queensland Border Declaration Pass system is not active. You do not need a border pass to enter Queensland.'}, 
+                    {'WA': 'You can enter Queensland from any Australian state or territory.There are no COVID-19 hotspots in Australia.The Queensland Border Declaration Pass system is not active. You do not need a border pass to enter Queensland.'},
+                    {'SA': 'You can enter Queensland from any Australian state or territory.There are no COVID-19 hotspots in Australia.The Queensland Border Declaration Pass system is not active. You do not need a border pass to enter Queensland.'},
+                    {'NT': 'You can enter Queensland from any Australian state or territory.There are no COVID-19 hotspots in Australia.The Queensland Border Declaration Pass system is not active. You do not need a border pass to enter Queensland.'},
+                    {'TAS': 'You can enter Queensland from any Australian state or territory.There are no COVID-19 hotspots in Australia.The Queensland Border Declaration Pass system is not active. You do not need a border pass to enter Queensland.'}
                 ])
                 setLink('https://www.covid19.qld.gov.au/government-actions/border-closing')
             } else if (state === 'NT') {
                 setRestrictions([
-                    {'ACT': 'ACT->NT'},
-                    {'NSW': 'NSW->NT'}, 
-                    {'VIC': 'VIC->NT'}, 
-                    {'QLD': 'QLD->NT'}, 
-                    {'WA': 'WA->NT'},
-                    {'SA': 'SA->NT'},
-                    {'TAS': 'TAS->NT'}
+                    {'ACT': 'All arrivals to the Northern Territory (NT) must: fill in a Border Entry Form and complete 14 days of mandatory supervised quarantine at your own expense*, if you have recently been in an active declared COVID-19 hot spot. This includes children returning from a hotspot.'},
+                    {'NSW': 'All arrivals to the Northern Territory (NT) must: fill in a Border Entry Form and complete 14 days of mandatory supervised quarantine at your own expense*, if you have recently been in an active declared COVID-19 hot spot. This includes children returning from a hotspot.'}, 
+                    {'VIC': 'All arrivals to the Northern Territory (NT) must: fill in a Border Entry Form and complete 14 days of mandatory supervised quarantine at your own expense*, if you have recently been in an active declared COVID-19 hot spot. This includes children returning from a hotspot.'}, 
+                    {'QLD': 'All arrivals to the Northern Territory (NT) must: fill in a Border Entry Form and complete 14 days of mandatory supervised quarantine at your own expense*, if you have recently been in an active declared COVID-19 hot spot. This includes children returning from a hotspot.'}, 
+                    {'WA': 'All arrivals to the Northern Territory (NT) must: fill in a Border Entry Form and complete 14 days of mandatory supervised quarantine at your own expense*, if you have recently been in an active declared COVID-19 hot spot. This includes children returning from a hotspot.'},
+                    {'SA': 'All arrivals to the Northern Territory (NT) must: fill in a Border Entry Form and complete 14 days of mandatory supervised quarantine at your own expense*, if you have recently been in an active declared COVID-19 hot spot. This includes children returning from a hotspot.'},
+                    {'TAS': 'All arrivals to the Northern Territory (NT) must: fill in a Border Entry Form and complete 14 days of mandatory supervised quarantine at your own expense*, if you have recently been in an active declared COVID-19 hot spot. This includes children returning from a hotspot.'}
                 ])
                 setLink('https://coronavirus.nt.gov.au/travel/quarantine')
             } else if (state === 'SA') {
                 setRestrictions([
-                    {'ACT': 'ACT->SA'},
-                    {'NSW': 'NSW->SA'}, 
-                    {'VIC': 'VIC->SA'}, 
-                    {'QLD': 'QLD->SA'}, 
-                    {'WA': 'WA->SA'},
-                    {'NT': 'NT->SA'},
-                    {'TAS': 'TAS->SA'}
+                    {'ACT': 'There are no current COVID-19 testing or quarantine restrictions for travellers from within Australia or New Zealand. Travellers are permitted to enter providing they have completed the Cross Border Travel Registration Form prior to entry into SA.'},
+                    {'NSW': 'There are no current COVID-19 testing or quarantine restrictions for travellers from within Australia or New Zealand. Travellers are permitted to enter providing they have completed the Cross Border Travel Registration Form prior to entry into SA.'}, 
+                    {'VIC': 'There are no current COVID-19 testing or quarantine restrictions for travellers from within Australia or New Zealand. Travellers are permitted to enter providing they have completed the Cross Border Travel Registration Form prior to entry into SA.'}, 
+                    {'QLD': 'There are no current COVID-19 testing or quarantine restrictions for travellers from within Australia or New Zealand. Travellers are permitted to enter providing they have completed the Cross Border Travel Registration Form prior to entry into SA.'}, 
+                    {'WA': 'There are no current COVID-19 testing or quarantine restrictions for travellers from within Australia or New Zealand. Travellers are permitted to enter providing they have completed the Cross Border Travel Registration Form prior to entry into SA.'},
+                    {'NT': 'There are no current COVID-19 testing or quarantine restrictions for travellers from within Australia or New Zealand. Travellers are permitted to enter providing they have completed the Cross Border Travel Registration Form prior to entry into SA.'},
+                    {'TAS': 'There are no current COVID-19 testing or quarantine restrictions for travellers from within Australia or New Zealand. Travellers are permitted to enter providing they have completed the Cross Border Travel Registration Form prior to entry into SA.'}
                 ])
                 setLink('https://www.covid-19.sa.gov.au/restrictions-and-responsibilities/travel-restrictions')
             } else if (state === 'WA') {
                 setRestrictions([
-                    {'ACT': 'ACT->WA'},
-                    {'NSW': 'NSW->WA'}, 
-                    {'VIC': 'VIC->WA'}, 
-                    {'QLD': 'QLD->WA'}, 
-                    {'SA': 'SA->WA'},
-                    {'NT': 'NT->WA'},
-                    {'TAS': 'TAS->WA'}
+                    {'ACT': 'All interstate arrivals must complete a G2G PASS declaration to enter WA.'},
+                    {'NSW': 'All interstate arrivals must complete a G2G PASS declaration to enter WA.'}, 
+                    {'VIC': 'All interstate arrivals must complete a G2G PASS declaration to enter WA.'}, 
+                    {'QLD': 'All interstate arrivals must complete a G2G PASS declaration to enter WA. Travellers from Queensland are permitted to enter WA without having to self-quarantine, but subject to strict conditions.'}, 
+                    {'SA': 'All interstate arrivals must complete a G2G PASS declaration to enter WA.'},
+                    {'NT': 'All interstate arrivals must complete a G2G PASS declaration to enter WA.'},
+                    {'TAS': 'All interstate arrivals must complete a G2G PASS declaration to enter WA.'}
                 ])
                 setLink('https://www.wa.gov.au/organisation/department-of-the-premier-and-cabinet/covid-19-coronavirus-travel-and-quarantine')
             } 
